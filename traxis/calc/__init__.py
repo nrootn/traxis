07fdc00cc95f56fcffa323edfa258c7103953342
Chris Dydula <chris@dydudx.com>
2014-12-15 02:20:30 -0500

    Add a copyright notice and statement of copying permission to each source file.

---
+++ traxis/calc/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2014 Syed Haider Abidi, Nooruddin Ahmed and Christopher Dydula
+#
+# This file is part of traxis.
+#
+# traxis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# traxis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with traxis.  If not, see <http://www.gnu.org/licenses/>.
